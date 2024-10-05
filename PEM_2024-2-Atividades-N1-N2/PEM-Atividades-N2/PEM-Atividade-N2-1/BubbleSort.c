5a88a5032794c6ff0407f28036662db894a19b9d
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2024-10-04 23:36:43 -0300

    Atividade-N2-1

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-1/BubbleSort.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-1/BubbleSort.c
@@ -9,14 +9,19 @@
 
 int quantidade;
 
+//Declaração de funções
 void entrada(int numeros[quantidade], int quantidade);
 void exibir(int numeros[quantidade], int quantidade);
 void ordenar(int numeros[quantidade], int quantidade);
 
 int main()
 {
+    //Pede a entrada de quantos números o array terá
     printf("Digite quantos números quer ordenar:\n");
     scanf("%i",&quantidade);
+
+    //Apresenta erro caso o número informado seja 1 ou menor
+    //Pois não é possível ordenar 1 elemento de mais de uma forma e não é possível ter número de elementos negativos
     if(quantidade <= 1)
     {
         printf("\nQuantidade de números deve ser maior que 1\n\n");
@@ -24,6 +29,7 @@ int main()
     }
     printf("\n");
     
+    //Define o array que será ordenado
     int numeros[quantidade];
     
     entrada(numeros, quantidade);
@@ -38,6 +44,7 @@ int main()
 
 void entrada(int numeros[quantidade], int quantidade)
 {
+    //Pede a entrada dos elementos do array
     printf("Digite os números a serem ordenados:\n");
     for(int i = 0;i<quantidade;i++)
     {
@@ -50,6 +57,7 @@ void entrada(int numeros[quantidade], int quantidade)
 
 void exibir(int numeros[quantidade], int quantidade)
 {
+    //Exibe os elementos do array
     for(int i = 0;i<quantidade;i++)
     {
         printf("|%i",numeros[i]);
@@ -64,16 +72,28 @@ void ordenar(int numeros[quantidade], int quantidade)
     int temporario, mudancas;
     mudancas = 1;
     
+    //Comando a ser executado enquanto a variável 'mudancas' for igual a 1
     while(mudancas == 1)
     {
+        //Define a variável 'mudancas' como 0, encerrando o loop 'while' caso não ocorra mudança de posição
         mudancas = 0;
+        //Percorre os itens do array comparando os elementos adjacentes e 
+        //colocando o menor na posição mais próxima do início e
+        //o maior na posição mais próxima do fim
         for(int i = 0;i<quantidade;i++)
         {
+            //Caso o número mais próximo do ínicio seja maior que o mais próximo do fim
+            //realiza a mudança de posições no array
             if(numeros[i] > numeros[i+1])
             {
+                //Utiliza a variavel 'temporario' para armazenar o valor do elemento maior
                 temporario = numeros[i];
+                //Joga o valor menor na posição mais próxima do início
                 numeros[i] = numeros[i+1];
+                //Joga o valor maior, armazenado na variavel 'temporario' para a posição mais próxima do fim
                 numeros[i+1] = temporario; 
+                //Define a variavel 'mudancas' como 1 para que o loop 'while' continue e o processo se repita
+                //até que o array inteiro esteja organizado
                 mudancas = 1;
             }
         }
